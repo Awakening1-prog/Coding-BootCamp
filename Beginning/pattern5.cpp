923d41b449fc5b4fd17518917c1456170398ede0
Utkarsh Srivastava <55698308+Awakening1-prog@users.noreply.github.com>
2021-02-09 21:17:22 +0530

    Create pattern5.cpp

---
+++ Beginning/pattern5.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char ** argv) {
+    int n;
+    cin >> n;
+    int c=1;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            cout<<c<<"	";
+            c+=1;
+        }
+        cout<<endl;
+    }
+    //write your code here
+
+}
+
+
+
+1	
+2	3	
+4	5	6	
+7	8	9	10	
+11	12	13	14	15
